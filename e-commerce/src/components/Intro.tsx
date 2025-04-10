27a411ceaecdb7e1f90f915bff6753ccea5363bb
IGABE Lydivine <lydigabem2018@gmail.com>
2025-04-10 22:51:32 +0200

    mm

--- e-commerce/src/components/Intro.tsx
+++ e-commerce/src/components/Intro.tsx
@@ -10,21 +10,14 @@ const Intro: React.FC = () => {
 
                 <div className='text-center md:text-left md:w-1/2'>
 
-<<<<<<< HEAD
-                    <h3 className='text-base md:text-5xl font-bold text-600 p-0 m-0 py-10'> Sleek $ Stylish </h3>
-                    <h2 className='text-3xl md:text-6xl font-extrabold text-orange-600 m-0 py'> Phone Case Collections 2025</h2>
-=======
+
                     <h3 className='text-base md:text-5xl font-bold p-0 m-0 py-10'> Sleek $ Stylish </h3>
                     <h2 className='text-3xl md:text-6xl font-extrabold text-orange-600 m-0'> Phone Case Collections 2025</h2>
->>>>>>> a8050f2648552cca018b8c773e7170bdb3385245
                     <p className='text-gray-600 text-base md:text-2xl mb-6 py-5 font-bold'> 
                         Protect your phone in style. Discover vibrant, durable cases made just for you.
                     </p>
+                    
                     <div className='flex flex-col sm:flex-row justify-center md:justify-start gap-4'>
-<<<<<<< HEAD
-                        
-=======
->>>>>>> a8050f2648552cca018b8c773e7170bdb3385245
                         <button className="bg-orange-600 text-white px-6 py-3  md:text-xl rounded-full font-semibold hover:bg-red-700 transition cursor-pointer">
                             Shop Now
                         </button>
