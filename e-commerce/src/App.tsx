88be8332173a013874cc501432dfdaf14dd5f5f0
Iradukunda Bonte <irabonte123@gmail.com>
2025-04-10 18:28:48 +0200

    latest component and cartContext

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
-
+import Latest from './components/Latest';
+import { CartProvider } from './components/CartContext'; 
 
 function App(){
   return(
    <main>
     <Navbar />  
     <Intro />
+    <CartProvider>
+      <div className="min-h-screen bg-gray-100">
+      <Latest />
+      </div>
+    </CartProvider>
    </main>
   )
 }
