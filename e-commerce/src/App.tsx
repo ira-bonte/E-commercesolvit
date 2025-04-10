5c814398f3d7550659f314a21fe1aeea61ab1ef1
IGABE Lydivine <lydigabem2018@gmail.com>
2025-04-10 10:52:39 +0200

    My App.tsx file

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css'
+import React from 'react';
+import Intro from './components/Intro';
 
 function App(){
-  return
+    return(
+        <main>
+            <Intro/>
+        </main>
+    );
 }
-  
+
 export default App;
