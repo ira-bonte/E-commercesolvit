ac5b0157f5c1385a1f7ec6e344d4aebd6160e1f9
IGABE Lydivine <lydigabem2018@gmail.com>
2025-04-10 22:44:31 +0200

    Just from main

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,17 +1,8 @@
-import './App.css';
-import NavBar from './components/Navbar';
-import Intro from './components/Intro';
 
-function App(){
-    return(
-        <main>
-            <NavBar />
-            <Intro/>
-        </main>
-    );
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import Latest from './components/Latest';
+import Popular from './components/Popular';
 import { CartProvider } from './components/CartContext'; 
 
 function App(){
@@ -19,6 +10,7 @@ function App(){
    <main>
     <Navbar />  
     <Intro />
+    <Popular />
     <CartProvider>
       <div className="min-h-screen bg-gray-100">
       <Latest />
